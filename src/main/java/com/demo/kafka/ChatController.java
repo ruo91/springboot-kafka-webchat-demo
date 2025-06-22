b4adf64038a0d047449e05862fce78dd1872b82b
ruo91 <ruo91@yongbok.net>
2025-06-22 12:55:57 +0900

    refactor: remove @SendTo annotation from message handler

--- src/main/java/com/demo/kafka/ChatController.java
+++ src/main/java/com/demo/kafka/ChatController.java
@@ -2,7 +2,6 @@ package com.demo.kafka;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
